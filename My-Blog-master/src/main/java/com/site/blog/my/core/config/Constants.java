82f8d98cb8528aaa562b04546b282ea7f3f6a33f
canda <995829376@qq.com>
2021-08-10 15:19:58 +0800

    git仓库测试提交

---
+++ My-Blog-master/src/main/java/com/site/blog/my/core/config/Constants.java
@@ -0,0 +1,9 @@
+package com.site.blog.my.core.config;
+
+/**
+ * Created by 13 on 2019/5/24.
+ */
+public class Constants {
+//    public final static String FILE_UPLOAD_DIC = "F:/project/blog/My-Blog-master/src/main/resources/templates/upload/";///opt/deploy/upload/ 上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "C:/upload/";///opt/deploy/upload/ 上传文件的默认url前缀，根据部署设置自行修改
+}
